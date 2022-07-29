761a21ff5ebe783d9357936221f2bb5faf9e1708
EthanJMe <91385100+EthanJMe@users.noreply.github.com>
2022-07-28 22:26:37 -0700

    read commment on line 57

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
-
+import { Items } from './Items';
+import { useState } from 'react';
 function App() {
+  const [runningTotal, setRunningTotal] = useState(0)
+  const [groceryList, setGroceryList] = useState([])
+  const [quantity, setQuantity] = useState(1)
+  const [searchQuery, setSearchQuery] = useState("")
+  const found = Items.find(obj => {
+    return obj.queryName === searchQuery.toLowerCase();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div><label>Search </label>
+    <input onChange={(e)=>{
+      setSearchQuery(e.target.value)
+    }}/>
+</div>
+
+<div>{found &&(
+  <div>
+    {found.name}: {found.stringPrice}
+    <Button onClick={()=>{
+        setRunningTotal(found.price*quantity+runningTotal) 
+        for(let i=0; i< quantity; i++){
+        groceryList.push(found)}
+      console.log(groceryList)}}
+        >Buy</Button>
+  </div>
+)}</div>
+
+    <label>Quantity Per Click</label>
+    <input onChange={(e)=>{
+      setQuantity(e.target.value)
+    }}/>
+
+    {
+      Items.map((item, index) => {
+      return(
+        <>
+        <div>{item.name}: {item.stringPrice}</div>
+        <Button onClick={()=>{
+        setRunningTotal(item.price*quantity+runningTotal) 
+        for(let i=0; i< quantity; i++){
+        groceryList.push(item)}
+      console.log(groceryList)}}
+        >Buy</Button>
+      </>
+      )
+    })}
+
+    <div className='cart'>
+      Cost of your cart: {runningTotal.toFixed(2)}
+
+    {groceryList.map((item, index)=>{
+      //you need to remove things last to first or it messes up the running total
+      return( 
+        <>
+        {item.name}
+        <Button onClick={()=>{groceryList.splice(groceryList.lastIndexOf(item.name))
+          setRunningTotal(runningTotal-item.price)
+        }}>Remove</Button>
+        </>
+        )
+      })}
+    
     </div>
+    
+    </>
   );
 }
 
---
+++ src/Items.js
@@ -0,0 +1,62 @@
+export const Items = [
+    {
+        name:' Milk',
+        queryName:'milk',
+        price: 2.99,
+        stringPrice:"2.99"
+    },
+    {
+        name:' Eggs',
+        queryName:'eggs',
+        price: 3.99,
+        stringPrice:"3.99"
+    },
+    {
+        name:' Bread',
+        queryName:'bread',
+        price: 2.25,
+        stringPrice:"2.25"
+    },
+    {
+        name:' Peanut Butter',
+        queryName:'peanut butter',
+        price: 4.25,
+        stringPrice:"4.25"
+    },    
+    {
+        name:' Jelly',
+        queryName:'jelly',
+        price: 3.50,
+        stringPrice:"3.50"
+    },
+    {
+        name:' Candy Bar',
+        queryName:'candy bar',
+        price: 0.75,
+        stringPrice:"0.75"
+    },
+    {
+        name:' Banana',
+        queryName:'banana',
+        price: 0.99,
+        stringPrice:"0.99"
+    },
+    {
+        name:' Cereal',
+        queryName:'cereal',
+        price: 7.99,
+        stringPrice:"7.99"
+    },
+    {
+        name:' Soda',
+        queryName:'soda',
+        price: 4.99,
+        stringPrice:"4.99"
+    },
+    {
+        name:' Ice Cream',
+        queryName:'ice cream',
+        price: 4.50,
+        stringPrice:"4.50"
+    },
+]
